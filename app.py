853a4c6e91c6c5207ab8ff1d6d452eef63e34d40
rainpasttime <2923336451@qq.com>
2018-07-05 11:38:49 +0800

    need to add the contact page

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+# -*-encoding:UTF-8 -*-
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
 
-if __name__=="__main__":
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
